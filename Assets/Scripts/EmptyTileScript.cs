06bd2e4901e402f63a1635c03fb280d036f2e9a1
Brainzy <nenadnikolic.tf@gmail.com>
2022-05-22 12:58:03 +0200

    GameBoard added

---
+++ Assets/Scripts/EmptyTileScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class EmptyTileScript : MonoBehaviour
+{
+	public EmptyTileScript upNeighbourTile;
+	public EmptyTileScript downNeighbourTile;
+	public EmptyTileScript rightNeighbourTile;
+	public EmptyTileScript leftNeighbourTile;
+	
+	
+	
+	
+}
